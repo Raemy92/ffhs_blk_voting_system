1adcb7a8994d4bf652c1ef8b9dc756bee9ae7a90
Laurent Raemy <raemy.laurent@gmail.com>
2023-01-04 11:14:01 +0100

    created basic components and some more dependencies

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
+import { HomeComponent } from './views/home/home.component'
+import { VoteComponent } from './views/vote/vote.component'
+import { ResultsComponent } from './views/results/results.component'
 
-const routes: Routes = []
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'vote', component: VoteComponent },
+  { path: 'results', component: ResultsComponent },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
+import { HomeComponent } from './views/home/home.component'
+import { CoreModule } from './core/core.module'
+import { VoteComponent } from './views/vote/vote.component'
+import { ResultsComponent } from './views/results/results.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    VoteComponent,
+    ResultsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
